60e4381ec41bd854cc5c3b59147ffb790528a64a
Hugo Firth <hugo.firth@gmail.com>
2015-04-19 17:21:33 +0300

    validating that passwords are equal

--- src/cljs/thoughtmule/views/register.cljs
+++ src/cljs/thoughtmule/views/register.cljs
@@ -9,12 +9,31 @@
             [thoughtmule.views.common :as c]
             [validateur.validation :as v :include-macros true]))
 
+(defn- attributes-equal
+  [attribute1 attribute2]
+  (fn [m]
+    (let [value1   (attribute1 m)
+          value2   (attribute2 m)
+          invalid (not (= value1 value2))]
+      (if invalid
+        {attribute1 #{(str " must equal " (name attribute2))}
+         attribute2 #{(str " must equal " (name attribute1))}}
+        nil))))
+
+(defn equal [field1 field2]
+  (let [validator (attributes-equal field1 field2)]
+    (fn [m]
+      (let [result (validator m)]
+        [(nil? result) result]))))
+
 (def User (v/validation-set
            (v/format-of :email
                         :format #"^.*@thoughtworks.com"
                         :message "Must be a valid email address")
            (v/length-of :password :within (range 6 100))
-           (v/presence-of :confirm-password)))
+           (v/presence-of :confirm-password)
+           (equal :password :confirm-password)
+           ))
 
 (defn handler [response]
   (.log js/console (str response)))
