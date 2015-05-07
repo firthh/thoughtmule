a8aa8ba2ee2df6d887713811be6a463a70022f61
Hugo Firth <hugo.firth@gmail.com>
2015-05-07 20:15:52 +0300

    copied equal validation to server side check

--- src/clj/thoughtmule/users.clj
+++ src/clj/thoughtmule/users.clj
@@ -5,13 +5,31 @@
             [ring.util.response :as response]
             [buddy.hashers :as hashers]))
 
+(defn- attributes-equal
+  [attribute1 attribute2]
+  (fn [m]
+    (let [value1   (attribute1 m)
+          value2   (attribute2 m)
+          invalid (not (= value1 value2))]
+      (if invalid
+        {attribute1 #{(str " must equal " (name attribute2))}
+         attribute2 #{(str " must equal " (name attribute1))}}
+        nil))))
+
+(defn equal [field1 field2]
+  (let [validator (attributes-equal field1 field2)]
+    (fn [m]
+      (let [result (validator m)]
+        [(nil? result) result]))))
+
 (def user-validation
   (validation-set
    (format-of :email
               :format #"^.*@thoughtworks.com$"
               :message "Must be a valid email address")
    (length-of :password :within (range 6 100))
-   (presence-of :confirm-password)))
+   (presence-of :confirm-password)
+   (equal :password :confirm-password)))
 
 (defprotocol UserProtocol
   (exists? [this email-address])
@@ -22,7 +40,7 @@
   (exists? [_ email-address]
     (:exist (first (db/user-exists? db-url email-address))))
   (register [this user]
-    (if (or (invalid? user-validation user) (not (= (:password user) (:confirm-password user))))
+    (if (invalid? user-validation user)
       (invalid {:message "not a valid user"})
       (if (.exists? this (:email user))
         (invalid {:message "user already exists"})
